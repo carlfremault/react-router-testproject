ff8f6ea1a74ff4f02f72483834cc12d29b47536d
Carl Fremault <carlfremault@yahoo.com>
2023-07-08 16:23:20 +0200

    deleting notes

--- frontend/src/components/NoteItem.tsx
+++ frontend/src/components/NoteItem.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, json, useNavigate } from 'react-router-dom';
 
 const NoteItem = ({ note }: { note: Note }) => {
+  const navigate = useNavigate();
+  const deleteNote = async () => {
+    const res = await fetch(`http://localhost:8080/notes/${note.id}`, {
+      method: 'DELETE',
+    });
+    if (!res.ok) {
+      throw json(
+        {
+          message: 'Could not delete note',
+        },
+        { status: 500 },
+      );
+    }
+    navigate('/notes');
+  };
+
   return (
     <article className="note-item">
       <span>
@@ -12,6 +27,7 @@ const NoteItem = ({ note }: { note: Note }) => {
       </span>
       <p>{note.content}</p>
       <NavLink to={`/notes/${note.id}`}>Edit</NavLink>
+      <button onClick={deleteNote}>Delete</button>
     </article>
   );
 };
